2e5e13a6e61cd2159f279753e961e217339a0cca
JorikDura <vandarkslave@outlook.com>
2025-09-30 02:16:16 +0300

    fix(api): require

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    require_once base_path('routes/v1/users.php');
+    require base_path('routes/v1/users.php');
 });
